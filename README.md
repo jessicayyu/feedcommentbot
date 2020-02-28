2feb229b5b796837180d5a32ce7e343443f44aa2
jessicayyu <jessyu2010@gmail.com>
2020-02-27 23:18:53 -0800

    Now able to read unread inbox messages. Readme updates.

---
+++ README.md
@@ -0,0 +1,27 @@
+# feed-comment-bot
+
+Tool that will manage Reddit user flair based on comment requests and cross-post new content to social media platforms.
+
+# Installing
+
+    npm install
+    npm start
+
+# Dependencies  
+* NodeJS
+* Snoowrap
+* Moment
+* dotenv
+* simple-oauth2
+
+# Helpful Links
+* Reddit API Documentation: https://www.reddit.com/dev/api/
+* Snoowrap Documentation: https://not-an-aardvark.github.io/snoowrap/index.html
+
+## Authors
+
+* **Jessica Yu** - [Github](https://github.com/jessicayyu)
+  
+## License
+  
+* No license.
--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const r = new snoowrap({
   password: process.env.REDDIT_PASS
 });
 
+
 var moment = require('moment');
 moment().format();
 
-r.getUnreadMessages({ limit: 5})
+r.getUnreadMessages({ limit: 3})
   .map((msg) => {
     let timestamp = moment.utc(msg.created_utc * 1000).local().format("ddd, MMM Do YYYY h:mmA");
     let truncated = msg.body.length > 150 ? msg.body.slice(1,150) : msg.body;
-    console.log(msg.author.name + ' ' + timestamp + ' ' + msg.name + '\n' + msg.body  + '\n')
+    console.log(msg.author.name + ' ' + timestamp + ' ' + msg.id + '\n' + truncated  + '\n');
   })
   .catch(console.error)
